d56c12d8139c8fe83eed422c1aa586a94c3a32e3
Lucas <keyruu@web.de>
2023-12-18 09:22:28 +0100

    ✨ sorry i have to do this

--- app.vue
+++ app.vue
@@ -11,6 +11,13 @@ useHead({
       type: 'image/png',
       href: '/favicon-32x32.png'
     }
+  ],
+  script: [
+    {
+      src: 'https://sorryihavetodothis.keyruu.de/js/script.js',
+      defer: true,
+      'data-domain': 'buymeaspezi.com'
+    },
   ]
 });
 
