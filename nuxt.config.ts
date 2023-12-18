e8749c16277c17d3802fa7439d0ad06e2326327a
Lucas <keyruu@web.de>
2023-12-18 17:10:17 +0100

    💄 do seo optimization and make img also show on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxt/ui",'@nuxtjs/i18n',],
-  ui: {
-    icons: ["mdi", "simple-icons"]
-  },
-  i18n: {
-    locales: [
-      {
-        code: 'de',
-        iso: 'de-DE',
-        name: 'German',
-        file: 'de.js'
-      },
-      {
-        code: 'en',
-        iso: 'en-US',
-        name: 'English',
-        file: 'en.js'
-      }
+    site: {
+        url: 'https://buymeaspezi.com',
+    },
+    devtools: {
+        enabled: true,
+
+        timeline: {
+            enabled: true,
+        },
+    },
+    modules: [
+        '@nuxt/ui',
+        '@nuxtjs/i18n',
+        '@nuxt/image',
+        'nuxt-simple-sitemap',
+        'nuxt-simple-robots',
     ],
-    defaultLocale: 'de',
-    langDir: 'lang/',
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: 'i18n_redirected',
-      redirectOn: 'root',  // recommended
+    ui: {
+        icons: ["mdi", "simple-icons"]
+    },
+    i18n: {
+        locales: [
+            {
+                code: 'de',
+                iso: 'de-DE',
+                name: 'German',
+                file: 'de.js'
+            },
+            {
+                code: 'en',
+                iso: 'en-US',
+                name: 'English',
+                file: 'en.js'
+            }
+        ],
+        defaultLocale: 'de',
+        langDir: 'lang/',
+        detectBrowserLanguage: {
+            useCookie: true,
+            cookieKey: 'i18n_redirected',
+            redirectOn: 'root',  // recommended
+        }
     }
-  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 const amount = ref(1)
-const buttonColors = 'text-gray-900 border border-gray-300 rounded-lg ' +
-    'hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white ' +
-    'dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
 
 function decrease() {
   if (amount.value > 1) amount.value--
@@ -12,7 +9,7 @@ function decrease() {
 <template>
   <section class="w-screen flex justify-center items-center h-full">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div class="mr-auto place-self-center lg:col-span-7">
+      <div class="order-2 mr-auto place-self-center lg:order-1 lg:col-span-7">
         <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
           <span class="wave-text">Paulaner Spezi</span> - {{ $t('tagLine') }}
           <span class="text-[#FFD700]">{{ $t('gold') }}</span>
@@ -35,15 +32,15 @@ function decrease() {
             </UButton>
           </div>
           <a target="_blank" :href="`https://paypal.me/lucaskeyruu/${amount}`">
-            <UButton icon="i-simple-icons-paypal" class="ml-4 px-5 py-3 rounded-lg text-transparent bg-gradient-to-r
+            <UButton icon="i-simple-icons-paypal" class="ml-4 px-5 py-3 rounded-lg bg-gradient-to-r
           from-paypal-400 to-paypal-600 hover:from-paypal-500 hover:to-paypal-700">
               {{ $t('paypal') }}
             </UButton>
           </a>
         </div>
       </div>
-      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-        <img class="object-contain" src="/paulanerspezi.png" alt="mockup">
+      <div class="order-1 flex justify-center px-24 lg:p-0 mb-6 lg:m-0 lg:order-2 lg:mt-0 lg:col-span-5">
+        <NuxtImg format="webp" class="object-contain" src="/paulanerspezi.png" alt="mockup" />
       </div>
     </div>
   </section>
@@ -55,22 +52,8 @@ function decrease() {
       90deg,
       #efd359, /* Light Yellow */ #df8051, /* Orange */ #d64f60, /* Red */ #ca468f, /* Pink */ #5c4693 /* Purple */
   );
-  //background-size: 100% 300%;
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
-  //animation: wave-animation 5s ease-in-out infinite;
-}
-
-@keyframes wave-animation {
-  0% {
-    background-position: 50% 0%;
-  }
-  50% {
-    background-position: 50% 100%;
-  }
-  100% {
-    background-position: 50% 0%;
-  }
 }
 </style>
